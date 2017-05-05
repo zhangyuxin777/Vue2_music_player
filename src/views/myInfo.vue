dd69b7792fa15c6e0449c19b4569b81667afc3d9
bruce <976490170@qq.com>
2017-05-05 18:11:49 +0800

    no message

--- src/views/myInfo.vue
+++ src/views/myInfo.vue
@@ -5,27 +5,34 @@
   <transition name="custom-classes-transition" enter-active-class="animated fadeInRight"
               leave-active-class="animated fadeOut" mode="out-in">
     <div class="my-info">
-      <div class="header-box">
-        <img
-          src="https://wx.qlogo.cn/mmopen/sMlrIA3MDEJV89ZgX8CO0dx9qjFicdiatUqBO7WkU1IznnDaZ2Vt43icgf59duojU1VDBlayzn0bgledbeGTmic5NGicevp0H5dz1/0"
-          alt="" class="header">
-        <ul class="list ul">
-          <li class="info-item">
-            <div class="i-title">作者</div>
-            <div class="i-con">Bruce Zhang</div>
-          </li>
-          <li class="info-item">
-            <div class="i-title">邮箱</div>
-            <div class="i-con">bruce777@foxmail.com</div>
-          </li>
-          <li class="info-item">
-            <div class="i-title">qq</div>
-            <div class="i-con">976490170</div>
-          </li>
-        </ul>
+      <div class="title-bar">
+        <span class="sprites ic_back"></span>
+        <span class="title">我</span>
+        <span class="sprites search"></span>
       </div>
+      <!--<div class="header-box">-->
+      <!--<img-->
+      <!--src="https://wx.qlogo.cn/mmopen/sMlrIA3MDEJV89ZgX8CO0dx9qjFicdiatUqBO7WkU1IznnDaZ2Vt43icgf59duojU1VDBlayzn0bgledbeGTmic5NGicevp0H5dz1/0"-->
+      <!--alt="" class="header">-->
+      <!--<ul class="list ul"></ul>-->
+      <!--</div>-->
       <span class="split-line"></span>
       <ul class="list">
+        <li class="info-item">
+          <div class="i-title">作者</div>
+          <div class="i-con">Bruce Zhang</div>
+          <span class="split-line"></span>
+        </li>
+        <li class="info-item">
+          <div class="i-title">邮箱</div>
+          <div class="i-con">bruce777@foxmail.com</div>
+          <span class="split-line"></span>
+        </li>
+        <li class="info-item">
+          <div class="i-title">qq</div>
+          <div class="i-con">976490170</div>
+          <span class="split-line"></span>
+        </li>
         <li class="info-item">
           <div class="i-title">我的博客</div>
           <span class="split-line"></span>
